cd6181a341e8f8dfd49a162172767e5df4641254
Chris Hart <chart@instructure.com>
2017-04-10 13:32:01 -0400

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "rules": {
+    "max-len": [2, 120, 2],
+    "jsx-quotes": [2, "prefer-double"],
+    "react/jsx-no-bind": [2, {
+      "ignoreRefs": true
+    }],
+    "key-spacing": [2, {
+      "beforeColon": false,
+      "afterColon": true,
+      "mode": "minimum"
+    }],
+    "no-var": 2,
+    "prefer-const": 2,
+    "no-param-reassign": ["error", { "props": true }],
+    "no-console": 2,
+    "no-useless-escape": 0,
+
+    "no-unused-expressions": 0,
+    "react/no-unused-prop-types": 0
+  },
+  "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+  ],
+  "extends": ["standard", "standard-react"]
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, PropTypes } from 'react'
+import Color from 'tinycolor2'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  static propTypes = {
+    color: PropTypes.string
+  }
+
+  render () {
+    const color = Color(this.props.color).desaturate(10)
+    const analogousColors = Color(color).analogous(7).map(function (t) {
+      return t.toHexString()
+    })
+    const headerStyles = {
+      background:
+        `radial-gradient(circle at 35% 90%, ${analogousColors[1]}, transparent 50%),
+        radial-gradient(circle at 43% 73%, ${analogousColors[2]}, transparent 60%),
+        radial-gradient(circle at 48% 86%, ${analogousColors[3]}, transparent 100%),
+        radial-gradient(circle at 73% 35%, ${analogousColors[4]}, transparent 100%),
+        radial-gradient(circle at 10% 0%, ${color}, transparent 100%),
+        radial-gradient(circle at 50% 50%, ${analogousColors[6]}, ${analogousColors[3]} 50%)`
+    }
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header" style={headerStyles}>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
@@ -14,8 +32,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
