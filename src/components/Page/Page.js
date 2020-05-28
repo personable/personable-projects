43a548eaee379ce7facea6846990790d18db4474
Chris Hart <chart@instructure.com>
2020-05-28 09:46:34 -0400

    upgrade deps

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import marked from "marked";
-import Background from "../Background/Background";
-import Checklist from "../Checklist/Checklist";
-import Image from "../Image/Image";
-import ProjectsActionLink from "../ProjectsActionLink/ProjectsActionLink";
-import ProjectsHeading from "../ProjectsHeading/ProjectsHeading";
-import Spinner from "../Spinner/Spinner";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import marked from 'marked'
+import Background from '../Background/Background'
+import Checklist from '../Checklist/Checklist'
+import Image from '../Image/Image'
+import ProjectsActionLink from '../ProjectsActionLink/ProjectsActionLink'
+import ProjectsHeading from '../ProjectsHeading/ProjectsHeading'
+import Spinner from '../Spinner/Spinner'
 
-import "./Page.css";
+import './Page.css'
 
 class Page extends Component {
   static propTypes = {
@@ -20,78 +20,78 @@ class Page extends Component {
     media: PropTypes.shape({
       src: PropTypes.string,
       img: PropTypes.string.isRequired,
-      alt: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired
     }),
     action: PropTypes.shape({
       src: PropTypes.string.isRequired,
       prompt: PropTypes.string.isRequired,
-      variant: PropTypes.string,
+      variant: PropTypes.string
     }),
     items: PropTypes.arrayOf(PropTypes.string),
     isCodePen: PropTypes.bool,
     isProject: PropTypes.bool,
     details: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.string),
-      PropTypes.node,
+      PropTypes.node
     ]),
     year: PropTypes.string,
     id: PropTypes.number,
-    desktopUI: PropTypes.bool,
+    desktopUI: PropTypes.bool
   };
 
   static defaultProps = {
-    mediaType: "image",
+    mediaType: 'image',
     isCodePen: false,
     isProject: false,
-    color: "rebeccapurple",
+    color: 'rebeccapurple'
   };
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     if (this.props.media.src) {
       this.state = {
         videoLoaded: false,
         videoAttempts: 0,
-        videoFailed: false,
-      };
+        videoFailed: false
+      }
     }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     // there are 3 animations using the --timing var fired BEFORE we want the video to play
     const timing = window
       .getComputedStyle(document.documentElement)
-      .getPropertyValue("--timing");
-    const timingNum = Number(timing.slice(0, -1));
-    const videoWaitTime = Math.round(timingNum * 3 * 100) * 10;
+      .getPropertyValue('--timing')
+    const timingNum = Number(timing.slice(0, -1))
+    const videoWaitTime = Math.round(timingNum * 3 * 100) * 10
 
-    document.title = this.props.title;
+    document.title = this.props.title
 
     if (this.props.media.src) {
       const timeout = setTimeout(() => {
         const timer = setInterval(() => {
-          this.handleVideo(timer);
-        }, 1000);
-        this.setState({ timer: timer });
-      }, videoWaitTime);
-      this.setState({ timeout: timeout });
+          this.handleVideo(timer)
+        }, 1000)
+        this.setState({ timer: timer })
+      }, videoWaitTime)
+      this.setState({ timeout: timeout })
     }
   }
 
-  componentWillUnmount() {
+  componentWillUnmount () {
     if (this.props.media.src) {
-      clearTimeout(this.state.timeout);
-      clearInterval(this.state.timer);
+      clearTimeout(this.state.timeout)
+      clearInterval(this.state.timer)
     }
   }
 
-  get headingID() {
-    return `ProjectsHeading${this.props.id}`;
+  get headingID () {
+    return `ProjectsHeading${this.props.id}`
   }
 
-  renderDetails() {
-    const details = this.props.details;
+  renderDetails () {
+    const details = this.props.details
     return details.map(function (detail, index) {
       return (
         <div
@@ -99,18 +99,18 @@ class Page extends Component {
           dangerouslySetInnerHTML={{ __html: marked(detail) }}
           className="PageDetail"
         />
-      );
-    });
+      )
+    })
   }
 
-  renderList() {
-    return <Checklist items={this.props.items} />;
+  renderList () {
+    return <Checklist items={this.props.items} />
   }
 
-  renderMedia() {
+  renderMedia () {
     if (this.props.media.src) {
       // If video won't load, just serve Image instead
-      const giveUpOnVideo = !this.state.videoLoaded && this.state.videoFailed;
+      const giveUpOnVideo = !this.state.videoLoaded && this.state.videoFailed
       return (
         <div className="PageVideoContainer">
           {!giveUpOnVideo ? (
@@ -123,8 +123,8 @@ class Page extends Component {
               poster={this.props.media.img}
               className={
                 this.state.videoLoaded
-                  ? "PageVideo PageVideo--loaded"
-                  : "PageVideo"
+                  ? 'PageVideo PageVideo--loaded'
+                  : 'PageVideo'
               }
             >
               <source src={this.props.media.src} type="video/mp4" />
@@ -132,7 +132,7 @@ class Page extends Component {
           ) : null}
           {this.renderVideoOverlay()}
         </div>
-      );
+      )
     } else {
       return (
         <a
@@ -142,65 +142,65 @@ class Page extends Component {
         >
           <Image src={this.props.media.img} alt={this.props.media.alt} />
         </a>
-      );
+      )
     }
   }
 
-  handleVideo(timer) {
-    const video = document.getElementById(`video${this.props.id}`);
+  handleVideo (timer) {
+    const video = document.getElementById(`video${this.props.id}`)
 
     // If the video is in the DOM, and ready to play...
     if (video && video.readyState > 3) {
       this.setState({
-        videoLoaded: true,
-      });
-      clearInterval(timer);
+        videoLoaded: true
+      })
+      clearInterval(timer)
       // If the video is in the DOM but not loaded yet...
     } else if (video && this.state.videoAttempts < 15) {
       this.setState({
-        videoAttempts: this.state.videoAttempts + 1,
-      });
+        videoAttempts: this.state.videoAttempts + 1
+      })
       // If the video is either not there or taking over 15 seconds...
     } else {
-      clearInterval(timer);
+      clearInterval(timer)
       this.setState({
-        videoFailed: true,
-      });
+        videoFailed: true
+      })
     }
   }
 
-  renderVideoOverlay() {
+  renderVideoOverlay () {
     if (!this.state.videoLoaded && !this.state.videoFailed) {
       return (
         <span className="PageVideoOverlay" aria-live="polite">
           <Spinner message={this.renderOverlayMessage()} color="light" />
         </span>
-      );
+      )
     } else if (!this.state.videoLoaded && this.state.videoFailed) {
-      return <Image src={this.props.media.img} alt={this.props.media.alt} />;
+      return <Image src={this.props.media.img} alt={this.props.media.alt} />
     } else {
-      return null;
+      return null
     }
   }
 
-  renderOverlayMessage() {
+  renderOverlayMessage () {
     if (this.state.videoAttempts < 14) {
-      return <span>Loading video&hellip;</span>;
+      return <span>Loading video&hellip;</span>
     } else {
-      return <span>Sorry! The video's not loading.</span>;
+      return <span>Sorry! The video's not loading.</span>
     }
   }
 
-  render() {
+  render () {
     // Update the active color CSS var based on the page content
     document.documentElement.style.setProperty(
-      "--color-active",
+      '--color-active',
       this.props.color
-    );
+    )
 
     return (
       <article
-        className={this.props.isProject ? "Page Page--isProject" : "Page"}
+        className={this.props.isProject ? 'Page Page--isProject' : 'Page'}
       >
         <Background
           color={this.props.backgroundColor || this.props.color}
@@ -245,8 +245,8 @@ class Page extends Component {
           </div>
         </div>
       </article>
-    );
+    )
   }
 }
 
-export default Page;
+export default Page
